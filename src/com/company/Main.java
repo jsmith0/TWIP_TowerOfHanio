770fb15c13a3d00ce2ed5e52a119645e62dc9514
jsmith0 <justins4@comcast.net>
2017-01-17 17:36:36 -0500

    3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I will solve the Tower Of Hanoi for you.");
-        System.out.println("The board looks like this \n" +
-                "   |   |   | \n___a___b___c___");
-        System.out.println("Enter the number of disks you would like.");
+        System.out.println("The board looks like this \n   |   |   | \n___a___b___c___");
+        System.out.println("Enter the number of disks you would like in the tower.");
+
         Scanner kb = new Scanner(System.in);
         int disks = kb.nextInt();
         hanoi.solve(disks, "a", "b", "c");
